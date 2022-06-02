a6d3a716f61b9133f979b6182f9e436501bb85f9
Avila Diego <aviladiego2009@gmail.com>
2022-06-02 13:18:07 -0300

    first

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import NavBar from './componentes/NavBar';
+import Contenedor from './componentes/Contenedor';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <main>
+      <Contenedor></Contenedor>
+      </main>
+
     </div>
   );
 }
---
+++ myapp/src/componentes/Puntaje.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Puntaje = () => {
+  return (
+    <div>Puntaje</div>
+  )
+}
+
+export default Puntaje
